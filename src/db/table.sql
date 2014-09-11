aa129e448a3485aa4298585655418a55a31af301
moratori <hoko1993@yahoo.co.jp>
2014-09-11 20:17:41 +0900

    unnecessary directories removed

--- src/db/table.sql
+++
@@ -1,6 +0,0 @@
-
-create table dmapper(d_id int primary key not null , name text , vtime int default 0);
-create table rmapper(r_id int primary key not null , d_id int  , path varchar(16384) default '/', vtime int default 0, counter int default 0);
-create table data(r_id int primary key not null , title text , data text , size int , hash varchar(64) , new boolean);
-create table linkr(r_id int, num int , child_r_id int );
-alter table linkr add constraint primary key(r_id,num);
