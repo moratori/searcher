eb2c48ae776c5de7b2117c06238f545c88e62753
moratori <hoko1993@yahoo.co.jp>
2014-10-15 15:05:19 +0900

    nmapperは消さないことにした

--- core/db/initindex.sql
+++ core/db/initindex.sql
@@ -1,13 +1,14 @@
 use searcher;
-drop table if exists nmapper;
+/*drop table if exists nmapper;*/
 drop table if exists freq;
 drop table if exists place;
-
 drop table if exists pagerank;
+
 
 update data set new = 1;
 
-create table nmapper(n_id int not null primary key auto_increment , noun text);
+
+/*create table nmapper(n_id int not null primary key auto_increment , noun text);*/
 create table freq(n_id int , r_id int , freq float ,tfidf float, tstamp int default 0 ,primary key(n_id , r_id));
 create table place (n_id int , r_id int , num int , place int , primary key(n_id , r_id , num));
 
