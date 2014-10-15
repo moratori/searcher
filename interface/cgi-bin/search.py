6682bb6877d0af9861a5ad90f657bfa70bf8b9c7
moratori <hoko1993@yahoo.co.jp>
2014-10-15 13:10:25 +0900

    見た目を作った

--- interface/cgi-bin/search.py
+++ interface/cgi-bin/search.py
@@ -18,36 +18,28 @@ METHOD = "GET"
 FOOTER = "</body></html>"
 
 FORM = """
-  <form action="/cgi-bin/search.py" method = "GET">
-    <input type = "radio" name = "domain" value = "0" %s>全て
-    <input type = "radio" name = "domain" value = "1" %s>電大トップ
-    <input type = "radio" name = "domain" value = "2" %s>SIE
-    <input type = "radio" name = "domain" value = "3" %s>CSE
-    <br><br>
-    <input size = "55" type = "text" name = "keyword">
-    <input type = "submit" value = "Search">
-  </form>
+  <div id = "sform">
+      <form action="/cgi-bin/search.py">
+        <input type="radio" name = "domain" value = "0" %s>全て
+        <input type="radio" name = "domain" value = "1" %s>電大トップ
+        <input type="radio" name = "domain" value = "2" %s>SIE
+        <input type="radio" name = "domain" value = "3" %s>CSE
+        <br><br>
+        <input type="text" name="keyword" class="word">
+        <input type="submit" value="検索" class="button">
+      </form>
+  </div>
 """
 
 HEADER = '''
 <html>
-<head>
-  <meta http-equiv = "Content-Type" content = "text/html; charset=utf8">
-  <title>results for %s</title>
-  <style type = "text/css">
-    <!--
-      #header {
-        text-align: center;  
-      }
-      #main{
-        text-align: center;  
-      }
-    -->
-  </style>
-</head>
-<body>
-  <div id = "header">
-  <h2>学内検索エンジン</h2>
+  <head>
+    <meta http-equiv="Conten-Type" content="text/html; charset=utf8">
+    <link rel="stylesheet" href="/style/sresult.css" type="text/css">
+    <title>大学内検索エンジン</title>
+  </head>
+  <body>
+    <div id = "header"><a href="http://web.dendai.ac.jp/" target="_blank"><img src="/img/logo.gif"></a></div>
 '''
 
 
@@ -61,7 +53,7 @@ print "Content-Type: text/html\n"
 def newline(data):
   s = u""
   for (num , c) in enumerate(data):
-    if num % 47 == 0 :
+    if num % 47 == 0 and num != 0:
       s += "<br>" + c
     else:
       s += c
@@ -89,6 +81,28 @@ def output_page(query , offset , now , domain):
   <a href = "/cgi-bin/search.py?keyword=%s&page=%s&domain=%s" style ="text-decoration: none;"><button type = "submit" style = "width: 32;height: 32;">%s</button>&nbsp;&nbsp;""" %(query.encode("utf-8") , offset , domain ,(("<b><font color = \"maroon\">" + str(offset) + "</font></b>") if (offset == now) else offset)))
 
 
+def output_buttons(enumnum,pageoff,hits,query,domain):
+  # 1 2 3 4 5 ... 30
+  # 2 3 4 5 6 7 ... 30
+  # 各ページへのリンク
+  pnum= hits / enumnum
+  rest = hits % enumnum
+  pnum = (pnum + 1) if rest != 0 else pnum 
+  # 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
+  # 分割表示数にする場合の数 sp
+  sp  = 15
+  # 前後の表示する数
+  bf  = 7
+  if (pnum > sp):
+    start = (pageoff - bf) if pageoff > bf else 1
+    end   = sp if (start == 1) else ((pageoff + bf) if pageoff < pnum - bf else pnum)
+    for n in range(start-1 , end):
+      output_page(query , n + 1 , pageoff , domain)
+  else:
+    for n in range(pnum): 
+      output_page(query , n + 1 , pageoff , domain)
+
+
 def search(query,pageoff,domain):
   # query は unicode
   enumnum = 9
@@ -98,40 +112,26 @@ def search(query,pageoff,domain):
   (res , hits) = c.search_and_toplevel(query , domstr , digestmaker = keyword_bold , pageoff=pageoff , default = enumnum)
   interval = time.time() - start
   if hits == 0:
-    print "<br>「<b>%s</b>」を含むウェブページは見つかりませんでした。</div>" %(query.encode("utf-8"))
+    print "<div id = \"info\">「<b>%s</b>」を含むウェブページは見つかりませんでした</div>" %(query.encode("utf-8"))
   else:
-    print "「<b>%s</b>」の検索結果 %s ページ目 %s件のヒット( %.4f 秒 ) <br><br></div><div id = \"main\">" %(query.encode("utf-8") , pageoff ,hits , interval)
+    print "<div id = \"info\">「<b>%s</b>」の検索結果 %s ページ目 %s件のヒット( %.4f 秒 )</div>" %(query.encode("utf-8") , pageoff ,hits , interval)
+
+    print "<div id = \"main\">"
     for (number , (url,title,data)) in enumerate(res):
       url = url.encode("utf-8")
       title = fixlen(title, 50 , dot = True).encode("utf-8")
       data = data.encode("utf-8")
-      print "<br>"
-      print "<a href = \"%s\" target = \"_blank\">%s</a><br>" %(url , "UNTITLED" if (title == "") else title)
-      print "<font size = \"2\" color = \"green\">%s</font>" %(fixlen(url , 60 , dot = True))
-      print "<font size = \"2\">%s</font>" %(data)
-      print "<br><br>"
-    print "<br>"
-    # 1 2 3 4 5 ... 30
-    # 2 3 4 5 6 7 ... 30
-    # 各ページへのリンク
-    pnum= hits / enumnum
-    rest = hits % enumnum
-    pnum = (pnum + 1) if rest != 0 else pnum 
-    # 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
-    # 分割表示数にする場合の数 sp
-    sp  = 15
-    # 前後の表示する数
-    bf  = 7
-    if (pnum > sp):
-      start = (pageoff - bf) if pageoff > bf else 1
-      end   = sp if (start == 1) else ((pageoff + bf) if pageoff < pnum - bf else pnum)
-      for n in range(start-1 , end):
-        output_page(query , n + 1 , pageoff , domain)
-    else:
-      for n in range(pnum): 
-        output_page(query , n + 1 , pageoff , domain)
+      print "<div class=\"each\">"
+      print "<div class=\"title\"><a href = \"%s\" target = \"_blank\">%s</a></div>" %(url , "UNTITLED" if (title == "") else title)
+      print "<div class=\"url\">%s</div>" %(fixlen(url , 60 , dot = True) )
+      print "<div class=\"abst\">%s</div>" %(data)
+      print "</div>"
+    print "</div>"
 
-    print "</div><br>"
+    print "<div id = \"pages\">"
+    output_buttons(enumnum , pageoff,hits,query,domain)
+    print "</div>"
+
   c.finish()
 
 
@@ -155,7 +155,7 @@ def main(environ):
   except:
     print NO_KEYWORD
     return
-  print (HEADER % query.encode("utf-8")) + checked_radio(FORM , domain)  + "<hr>"
+  print HEADER + checked_radio(FORM , domain)
   search(query,page,domain)
   print FOOTER
 
